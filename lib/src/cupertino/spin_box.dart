6fb3880818d31683375fec1de249546c0a87081c
leonardo2204 <ferrari2204_5@hotmail.com>
2021-12-20 10:47:18 -0300

    Remove wrong material import in cupertino spin_box

--- lib/src/cupertino/spin_box.dart
+++ lib/src/cupertino/spin_box.dart
@@ -21,7 +21,6 @@
 // SOFTWARE.
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 
